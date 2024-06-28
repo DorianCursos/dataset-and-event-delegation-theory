4efecd1db53904b0d4b9672c29d3d1c8ecd90876
Dorian <doriancursos@gmail.com>
2024-06-28 11:41:03 +0200

    finish

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,24 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+// DELEGACIÓN DE EVENTOS
+// DATASET
+// ACCESO A OBJETOS MÚLTIPLES
+
+const pets = {
+  cat1: {
+    name: 'Anubis',
+    age: 9
+  },
+  cat2: {
+    name: 'Nala',
+    age: 6
+  }
+};
+
+const printValue = (pet, property) => {
+  console.log(pets[pet][property]);
+};
+
+printValue('cat1', 'name');
+printValue('cat2', 'age');
